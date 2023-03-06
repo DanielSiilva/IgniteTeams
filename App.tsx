596d96ebee6a743547b07c2049a2a974a0b31b8b
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-06 13:05:24 -0300

    Feat: CSS-in-JS - Componente de Loading

--- App.tsx
+++ App.tsx
@@ -6,13 +6,14 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import theme from './src/theme';
 
 import  Groups  from '@screens/Groups';
+import  {Loading}  from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
+      { !fontsLoaded ? <Groups/> : <Loading /> }
     </ThemeProvider>
   );
 }
