2a5098fad2acad6ed582e289a0f379656d745f41
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-06 19:47:45 -0300

    Feat: Criando Componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import LogoImg from '@assets/logo.png'
+
+import {
+    Container,
+    Logo
+} from './styles'
+
+export function Header (){
+
+    return (
+        <Container>
+            <Logo  source={LogoImg}/>
+        </Container>
+    )
+}
