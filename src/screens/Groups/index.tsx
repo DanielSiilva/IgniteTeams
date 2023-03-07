df2811bb9fde942ded90ed720e1ce0ad7b442cc8
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-06 22:19:18 -0300

    Feat: CSS-in-JS - Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,19 @@
+import {useState} from 'react';
+import {FlatList} from 'react-native'
+
 import { GroupCard } from '@components/GrupsCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+
 
 import {
   Container, 
 } from './styles'
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera a igreja', 'Galera do trabalho'])
+
+
   return (
     <Container >
       <Header/>
@@ -16,9 +23,17 @@ export default function Groups() {
         subtitle="jogue com  sua turma"
       />
 
-      <GroupCard 
-        title='Galera do Ignite'
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem= {({item})=>(
+          <GroupCard 
+            title={item}
+          />
+        )}
+      
       />
+
 
     </Container>
   );
