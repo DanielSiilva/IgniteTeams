846a0243a745a1678fb149a724c4e6124fecf521
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-08 20:58:09 -0300

    Feat: Navegação - Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,12 +7,15 @@ import { Container } from './styles';
 import { ListEmpty } from '@components/ListEmpty';
 import { GroupCard } from '@components/GroupsCard';
 import {Button} from '@components/Button'
+import { useNavigation } from '@react-navigation/native';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
+  const navigation = useNavigation()
+
   function handleNewGroup(){
-    
+    navigation.navigate('new')
   }
 
   return (
