eb950291ca4ed6e57e8c313a5ce27885952a8c8d
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-08 20:09:55 -0300

    Feat: Navegação - Estrutura de Navegação

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import theme from './src/theme';
 
-import {Players} from '@screens/Players'
 import  {Loading}  from '@components/Loading';
+
+import {Routes} from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Players/> : <Loading /> }
+      {fontsLoaded ? <Routes/> : <Loading /> }
     </ThemeProvider>
   );
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,26 +1,27 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {Groups} from '@screens/Groups'
-import {NewGroup} from '@screens/NewGroup'
-import {Players} from '@screens/Players'
+import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
+import { NewGroup } from '@screens/NewGroup';
 
-const {Navigator, Screen} = createNativeStackNavigator()
+
+const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes(){
-    <Navigator>
-        <Screen
-            name='groups'
-            component={Groups}
-        />
-
-        <Screen
-            name='new'
-            component={NewGroup}
-        />
-
-        <Screen
-            name='players'
-            component={Players}
-        />
+  return(
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen 
+        name="groups"
+        component={Groups}
+      />
+      <Screen 
+        name="new"
+        component={NewGroup}
+      />
+      <Screen 
+        name="players"
+        component={Players}
+      />
     </Navigator>
+  );
 }
